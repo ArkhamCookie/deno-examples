a039e76efab60dc23fe112faac4ec97d19f7677b
ArkhamCookie <git@mail.theuntitledproject.dev>
2023-06-09 19:14:38 -0500

    chat app stage 1

---
+++ src/chat/public/app.js
@@ -0,0 +1,49 @@
+const myUsername = prompt('Please enter your name') || 'Anonymous';
+// const port = 8080
+// const socket = new WebSocket('ws://localhost:' + port + 'start_web?username=' + myUsername)
+const socket = new WebSocket(`ws://localhost:8080/start_web_socket?username=${myUsername}`)
+
+/*
+socket.onmessage = (m) => {
+	const data = JSON.parse(m.data);
+
+	switch (data.event) {
+		case 'update-users':
+			let userListHtml = ''
+			for (const username of data.usernames) {
+				userListHtml += '<div> ' + username + ' </div>'
+			}
+			document.getElementById('users').innerHTML = userListHtml
+			break
+
+		case 'send-message':
+			addMessage(data.username, data.message)
+			break
+		}
+}
+*/
+
+/*
+function addMessage(username, message) {
+	document.getElementById(
+		"conversation"
+	).innerHTML += `<b> ${username} </b>: ${message} <br/>`;
+}
+*/
+
+window.onload = () => {
+	document.getElementById('data').addEventListener('keypress', (e) => {
+		if (e.key === 'Enter') {
+			const inputElement = document.getElementById("data");
+			const message = inputElement.value;
+			inputElement.value = "";
+			// socket.send(JSON.stringify({ event: 'send-message', message: message }))
+			socket.send(
+				JSON.stringify({
+					event: 'send-message',
+					message: message
+				})
+			)
+		}
+	})
+}
